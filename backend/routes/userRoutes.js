4b41d7580ed974c659185a383e8aa627c7413471
reysanmadamba <96415106+reysanmadamba@users.noreply.github.com>
2025-05-10 12:58:28 -0600

    created .gitignore and added backend/.env for the password security

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,13 +6,13 @@ const jwt = require('jwtwebtoken')
 
 router.post('/register', async (req, res) => {
     try {
-        const {username, email, password} = req.body;
+        const { username, email, password } = req.body;
 
-       
+
 
         const existingUser = await User, findOne({ email });
         if (existingUser) {
-            return res.status(400).json({message: 'Email already in user'});
+            return res.status(400).json({ message: 'Email already in user' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -25,31 +25,54 @@ router.post('/register', async (req, res) => {
 
         await newUser.save();
 
-        res.status(201).json({ message: 'User registered successfully'})
-        
+        res.status(201).json({ message: 'User registered successfully' })
+
     }
 
     catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 })
 
 router.post('./login', async (req, res) => {
 
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
-        const user = await User, findOne({email});
+        const user = await User, findOne({ email });
         if (!user) {
-            return res.status(400).json({message: 'User not found'})
+            return res.status(400).json({ message: 'User not found' })
         }
 
-        const isMatch = await bcrypt.compare(passwordm user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json()
+            return res.status(400).json({ message: 'User not found' })
         }
+   
+
+
+
+    const token = jwt.sign({id: user._id }, process.env.JWT_SECRET, {expiresIn: '1d'})
+
+    res.json({
+        token,
+        user: {
+            id: user_id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+    catch (err) {
+        res.status(400).json({ error: err.message })
+
     }
 
 
 
+
+
 })
+
+module.export = router;
