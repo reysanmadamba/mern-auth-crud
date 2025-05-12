8413e10487c1f24f5a35c90d739b8b1c7106f0fd
reysanmadamba <96415106+reysanmadamba@users.noreply.github.com>
2025-05-12 15:23:03 -0600

    created a component PrivateRoute to protect routes that require authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginForm from './components/LoginForm'
 import Profile from './components/Profile'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/profile" element={<Profile />}/>
+        <Route path="/login" element={<LoginForm />} />
 
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+
+        </Route>
       </Routes>
 
     </BrowserRouter>
---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import {Navigate, Outlet } from "react-router-dom"
+
+
+const PrivateRoute = () => {
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+        return <Navigate to="/login" replace/>
+    }
+    return <Outlet />
+}
+
+export default PrivateRoute;
+
+
