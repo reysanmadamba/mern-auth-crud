7b019d2e7374049a3de5d9472ab454fabdd0a4fa
reysanmadamba <96415106+reysanmadamba@users.noreply.github.com>
2025-05-13 09:12:33 -0600

    created a note model, now starting to make the crud

---
+++ backend/models/Note.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+
+const noteSchema = new mongoose.Schema( {
+userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+},
+
+title: {
+    type: String,
+    required: true
+},
+
+content: String,
+createdAt: {
+    type: Date,
+    default: Date.now
+}
+
+
+})
+
+module.exports = mongoose.model('Note', noteSchema);
